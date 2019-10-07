cef80bd042c6281b7dbbdfbcc04781905d4082dd
Cyril Verluise <cyril.verluise@gmail.com>
2019-10-07 21:56:08 +0200

    Add class config

--- techlandscape/utils.py
+++ techlandscape/utils.py
@@ -1,3 +1,6 @@
+import json
+from google.cloud import bigquery as bq
+
 english_speaking_offices = [
     "AP",
     "AU",
@@ -27,6 +30,8 @@ def format_table_ref_for_bq(table_ref):
 def country_clause_for_bq():
     """
     Return the list of countries of interest in a bq understandable way
+    Resource: https://worldwide.espacenet.com/help?locale=en_EP&method=handleHelpTopic&topic
+    =countrycodes
     :return: str '"AP","AU","CA",...'
     """
     return ",".join(
@@ -41,3 +46,28 @@ def flatten(l):
     :return: list
     """
     return [item for sublist in l for item in sublist if item]
+
+
+class Config:
+    def __init__(self):
+        self.config_dict = json.load(open("config.json", "rb"))
+        self.project_id = self.config_dict["project_id"]
+        self.dataset_id = self.config_dict["dataset_id"]
+
+    def client(self):
+        """
+        :return:
+        """
+        return bq.Client(project=self.project_id)
+
+    def table_ref(self, table_id, client=None):
+        """
+        :param client: bq.Client or None, if None, populated with config.json attr
+        :param table_id: str
+        :return: table_ref
+        """
+        if not client:
+            client = self.client()
+        return client.dataset(dataset_id=self.dataset_id).table(
+            table_id=table_id
+        )
