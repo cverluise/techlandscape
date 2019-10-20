b1cf4ba4e28a8ce00d3ed7086c7cd5708283d5d7
Cyril Verluise <cyril.verluise@gmail.com>
2019-10-20 15:55:30 +0200

    :art: Refacto: split data preparation and model training

--- techlandscape/pruning/train_model.py
+++ techlandscape/pruning/train_model.py
@@ -9,30 +9,20 @@ from techlandscape.pruning import vectorize_text, build_model
 # TODO? implement EarlyStopping on f_score
 
 
-def train_mlp(
-    texts_train,
-    texts_test,
-    y_train,
-    y_test,
-    learning_rate=1e-3,
-    epochs=100,
-    batch_size=64,
-    layers=2,
-    units=64,
-    dropout_rate=0.2,
-):
+def train_mlp(texts_train, texts_test, y_train, y_test, params):
     """
     Return the trained MLP model.
     :param texts_train: List[str], list of training set texts.
     :param texts_test: List[str], list of test set texts.
     :param y_train: arr, training set labels.
     :param y_test: arr, test set labels.
-    :param learning_rate: float, learning rate.
-    :param epochs: int, number of epochs to train the model.
-    :param batch_size: int, number of samples per gradient update.
-    :param layers: int, number of hidden layers -1. If 1, then no hidden layer.
-    :param units: int, number of units per hidden layer.
-    :param dropout_rate: float [0,1], fraction of the input units to drop.
+    :param params: dict, model hyper-parameters. Hyper-parameters are:
+            - learning_rate: float, learning rate.
+            - epochs: int, number of epochs to train the model.
+            - batch_size: int, number of samples per gradient update.
+            - layers: int, number of hidden layers -1. If 1, then no hidden layer.
+            - units: int, number of units per hidden layer.
+            - dropout_rate: float [0,1], fraction of the input units to drop.
     :return: fitted model, model history
     """
 
@@ -43,12 +33,12 @@ def train_mlp(
 
     # Create model instance.
     model = build_model.build_mlp(
-        layers=layers,
-        units=units,
-        dropout_rate=dropout_rate,
+        layers=params["layers"],
+        units=params["units"],
+        dropout_rate=params["dropout_rate"],
         input_shape=x_train.shape[1:],
     )
-    optimizer = Adam(lr=learning_rate)
+    optimizer = Adam(lr=params["learning_rate"])
     model.compile(
         optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"]
     )
@@ -61,11 +51,11 @@ def train_mlp(
     history = model.fit(
         x_train,
         y_train,
-        epochs=epochs,
+        epochs=params["epochs"],
         callbacks=callbacks,
         validation_data=(x_test, y_test),
         verbose=2,  # Logs once per epoch.
-        batch_size=batch_size,
+        batch_size=params["batch_size"],
     )
 
     # Print results.
@@ -79,63 +69,50 @@ def train_mlp(
     return model, history
 
 
-def train_cnn(
-    texts_train,
-    texts_test,
-    y_train,
-    y_test,
-    learning_rate=1e-3,
-    epochs=100,
-    batch_size=64,
-    blocks=2,
-    filters=64,
-    dropout_rate=0.2,
-    embedding_dim=200,
-    kernel_size=3,
-    pool_size=3,
-):
+def train_cnn(texts_train, texts_test, y_train, y_test, params):
     """
 
     :param texts_train: List[str], list of training set texts.
     :param texts_test: List[str], list of test set texts.
     :param y_train: arr, training set labels.
     :param y_test: arr, test set labels.
-    :param learning_rate: float, learning rate.
-    :param epochs: int, number of epochs to train the model.
-    :param batch_size: int, number of samples per gradient update.
-    :param blocks: int, number of Convolution-Pooling pairs.
-    :param filters: int, the dimensionality of the output space (i.e. the number of output
+    :param params: dict, model hyper-parameters. Hyper-parameters are:
+            - learning_rate: float, learning rate.
+            - epochs: int, number of epochs to train the model.
+            - batch_size: int, number of samples per gradient update.
+            - blocks: int, number of Convolution-Pooling pairs.
+            - filters: int, the dimensionality of the output space (i.e. the number of output
     filters in the convolution).
-    :param dropout_rate: float, percentage of input to drop at Dropout Layer.
-    :param embedding_dim: int, dimension of the embedding vectors (reco between 50 and 200).
-    :param kernel_size: int, length of the 1D convolution window.
-    :param pool_size: int, factor by which to downscale input at MaxPooling layer.
+            - dropout_rate: float, percentage of input to drop at Dropout Layer.
+            - embedding_dim: int, dimension of the embedding vectors (reco between 50 and 200).
+            - kernel_size: int, length of the 1D convolution window.
+            - pool_size: int, factor by which to downscale input at MaxPooling layer.
     :return: fitted model, model history
     """
     # Vectorize texts.
-    x_train, x_test, word_index = vectorize_text.get_sequence(
+    x_train, x_test, tokenizer = vectorize_text.get_sequence(
         texts_train, texts_test
     )
-    num_features = min(len(word_index) + 1, vectorize_text.TOP_K)
+    num_features = min(len(tokenizer.word_index) + 1, vectorize_text.TOP_K)
 
     # Buid models.
     model = build_model.build_cnn(
-        blocks=blocks,
-        filters=filters,
-        kernel_size=kernel_size,
-        embedding_dim=embedding_dim,
-        dropout_rate=dropout_rate,
-        pool_size=pool_size,
+        blocks=params["blocks"],
+        filters=params["filters"],
+        kernel_size=params["kernel_size"],
+        embedding_dim=params["embedding_dim"],
+        dropout_rate=params["dropout_rate"],
+        pool_size=params["pool_size"],
         input_shape=x_train.shape[1:],
         num_features=num_features,
     )
 
     # Compile model with learning parameters.
-    optimizer = Adam(lr=learning_rate)
+    optimizer = Adam(lr=params["learning_rate"])
     model.compile(
         optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"]
     )
-
+    # TODO implement grid search from good practices reported in Zhang and Wallace
     # Create callback for early stopping on validation loss. If the loss does
     # not decrease in two consecutive tries, stop training.
     callbacks = [EarlyStopping(monitor="val_loss", patience=2)]
@@ -144,11 +121,11 @@ def train_cnn(
     history = model.fit(
         x_train,
         y_train,
-        epochs=epochs,
+        epochs=params["epochs"],
         callbacks=callbacks,
         validation_data=(x_test, y_test),
         verbose=2,  # Logs once per epoch.
-        batch_size=batch_size,
+        batch_size=params["batch_size"],
     )
 
     # Print results.
