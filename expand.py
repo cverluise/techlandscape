3a38247e90900dd3a6415ea32f6b61be63b62662
Cyril <cyril@ipwe.com>
2019-12-01 21:20:55 +0100

    :art: Integrate new features from 58a3bab72ea7742e

--- expand.py
+++ expand.py
@@ -15,12 +15,28 @@ from techlandscape.expansion.expansion import (
 )
 from techlandscape.expansion.io import load_to_bq, get_expansion_result
 from techlandscape.expansion.utils import get_antiseed_size
-from techlandscape.utils import breathe
 
 
 def full_expansion(
-    seed_file, table_name, data_path, pc_flavor, bq_config=None
+    seed_file,
+    table_name,
+    data_path,
+    pc_flavor,
+    bq_config=None,
+    pc_counterfactual_f=None,
+    countries=None,
 ):
+    """
+    
+    :param seed_file:
+    :param table_name:
+    :param data_path:
+    :param pc_flavor:
+    :param bq_config:
+    :param pc_counterfactual_f:
+    :param countries:
+    :return:
+    """
     # TODO check that the expansion is already persisted
 
     if os.path.isfile(f"{data_path}{table_name}_classif.csv.gz"):
@@ -38,19 +54,19 @@ def full_expansion(
 
         load_to_bq(seed_file, client, table_ref, load_job_config)
 
-        breathe()
-        important_pc = get_important_pc(pc_flavor, 2, client, table_ref)
+        important_pc = get_important_pc(
+            pc_flavor, 50, client, table_ref, pc_counterfactual_f, countries
+        )
         important_pc.to_csv(
             data_path + f"{table_name}_important_pc.csv.gz", compression="gzip"
         )
         important_pc_sub = list(
-            important_pc.sort_values(by="odds", ascending=False)
-            .iloc[:50]
-            .index
+            important_pc.sort_values(by="odds", ascending=False).index
         )
-        pc_expansion(pc_flavor, important_pc_sub, client, query_job_config)
-
-        breathe()
+        pc_expansion(
+            pc_flavor, important_pc_sub[:50], client, query_job_config
+        )
+        # we restrict to the top 50
         citation_expansion(
             "citation", "L1", client, table_ref, query_job_config
         )
@@ -58,7 +74,6 @@ def full_expansion(
             "cited_by", "L1", client, table_ref, query_job_config
         )
 
-        breathe()
         citation_expansion(
             "citation", "L2", client, table_ref, query_job_config
         )
@@ -68,14 +83,17 @@ def full_expansion(
 
         seed_size = len(np.loadtxt(seed_file, dtype="str")) - 1
         antiseed_size = int(get_antiseed_size(seed_size) / 2)
-        draw_af_antiseed(antiseed_size, client, table_ref, query_job_config)
+        draw_af_antiseed(
+            antiseed_size, client, table_ref, query_job_config, countries
+        )
         draw_aug_antiseed(
             antiseed_size,
             pc_flavor,
-            important_pc_sub,
+            important_pc_sub,  # we exclude _all_ important pcs
             client,
             table_ref,
             query_job_config,
+            countries,
         )
 
         classif_df = get_expansion_result("*seed", client, table_ref)
@@ -105,6 +123,17 @@ if __name__ == "__main__":
         "--pc_flavor",
         help="Patent class flavor for patent class expansion. Currently "
         "supported: 'ipc', 'cpc'",
+    )
+    @click.option(
+        "--pc_counterfactual_f",
+        default=None,
+        help="path to the csv.gz file with universe pc freq (country_code|year|pc|freq)",
+    )
+    @click.option(
+        "--countries",
+        default=None,
+        type=list,
+        help="List of ISO2 countries we are interested in. E.g. ['US', 'CA']",
     )
     def main(seed_file, table_name, data_path, pc_flavor):
         full_expansion(seed_file, table_name, data_path, pc_flavor)
