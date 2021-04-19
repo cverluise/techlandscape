a181abd69c54f28830ab223074eb45fe2b205180
Cyril Verluise <cyril.verluise@gmail.com>
2021-04-19 15:00:38 +0200

    :art: migrate model prep data to utils (flatten code structure)

--- techlandscape/utils.py
+++ techlandscape/utils.py
@@ -3,10 +3,12 @@ import string
 import json
 import typer
 import pandas as pd
+import yaml
 from pathlib import Path
-from typing import List
+from typing import List, Tuple, Any
 from google.cloud import bigquery
 from google.oauth2 import service_account
+from sklearn.model_selection import train_test_split
 from techlandscape.lib import ISO2CNT
 from techlandscape.decorators import timer
 from techlandscape.exception import SmallSeed, SMALL_SEED_MSG
@@ -15,15 +17,13 @@ ok = "\u2713"
 not_ok = "\u2717"
 
 
-def get_bq_client(credentials: Path):
+def get_bq_client(credentials: Path) -> bigquery.Client:
     """"""
     credentials = service_account.Credentials.from_service_account_file(
         credentials, scopes=["https://www.googleapis.com/auth/cloud-platform"]
     )
 
-    client = bigquery.Client(
-        credentials=credentials, project=credentials.project_id
-    )
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
     return client
 
 
@@ -38,9 +38,7 @@ def get_bq_job_done(
     """Config and execute query job.
     Nb: `**kwargs` passed to bigquery.QueryJobConfig"""
     job_config = bigquery.QueryJobConfig(
-        destination=destination_table,
-        write_disposition=write_disposition,
-        **kwargs,
+        destination=destination_table, write_disposition=write_disposition, **kwargs
     )
     client = get_bq_client(credentials)
     if verbose:
@@ -48,8 +46,7 @@ def get_bq_job_done(
     client.query(query, job_config=job_config).result()
 
     typer.secho(
-        f"{ok}Query results saved to {destination_table}",
-        fg=typer.colors.GREEN,
+        f"{ok}Query results saved to {destination_table}", fg=typer.colors.GREEN
     )
 
 
@@ -65,8 +62,7 @@ def iso2cnt(ent_list: List[str], flavor: str) -> List[str]:
     nb_missing = len(ent_list) - len(res)
     if nb_missing > 0:
         typer.secho(
-            f"Unable to find {nb_missing} entity.ies.",
-            color=typer.colors.YELLOW,
+            f"Unable to find {nb_missing} entity.ies.", color=typer.colors.YELLOW
         )
         if nb_missing == len(ent_list):
             typer.secho(
@@ -137,42 +133,26 @@ def get_country_prefix(key: str) -> str:
     Return a prefix to unnest country field for tables at the family level (else empty)
     """
     assert key in ["publication_number", "family_id"]
-    return (
-        "" if key == "publication_number" else ", UNNEST(country) as country"
-    )
+    return "" if key == "publication_number" else ", UNNEST(country) as country"
 
 
 def get_country_clause(countries: List[str]) -> str:
     """
     Return a restrictive clause on countries if `countries` not None
     """
-    return (
-        f"AND country in ({get_country_string_bq(countries)})"
-        if countries
-        else ""
-    )
+    return f"AND country in ({get_country_string_bq(countries)})" if countries else ""
 
 
-def get_pc_like_clause(
-    flavor: str, pc_list: List[str], sub_group: bool = False
-):
+def get_pc_like_clause(flavor: str, pc_list: List[str], sub_group: bool = False):
     """
     Return a close to restrict to pc.code which contain at least one of the pc codes in pc_list
     """
     assert flavor in ["cpc", "ipc"]
-    pc_list_ = (
-        list(map(lambda x: x.split("/")[0], pc_list)) if sub_group else pc_list
-    )
+    pc_list_ = list(map(lambda x: x.split("/")[0], pc_list)) if sub_group else pc_list
     return (
         "("
         + " OR ".join(
-            set(
-                list(
-                    map(
-                        lambda x: f'{flavor}.code LIKE "%' + x + '%"', pc_list_
-                    )
-                )
-            )
+            set(list(map(lambda x: f'{flavor}.code LIKE "%' + x + '%"', pc_list_)))
         )
         + ")"
     )
@@ -189,9 +169,7 @@ def get_antiseed_size(
     if seed_size < min_size:
         raise SmallSeed("Danger Zone: your seed is too small. Don't cross!")
     elif min_size <= seed_size < threshold_size:
-        share_seed = (
-            0.1 + (seed_size - min_size) / (threshold_size - min_size) * 0.1
-        )
+        share_seed = 0.1 + (seed_size - min_size) / (threshold_size - min_size) * 0.1
         antiseed_size = int(seed_size / share_seed)
     else:
         share_seed = 0.2
@@ -228,9 +206,61 @@ def get_share_test(
     if seed_size < min_size:
         raise SmallSeed(SMALL_SEED_MSG)
     elif min_size <= seed_size < threshold_size:
-        share_test = (
-            0.5 - (seed_size - min_size) / (threshold_size - min_size) * 0.3
-        )
+        share_test = 0.5 - (seed_size - min_size) / (threshold_size - min_size) * 0.3
     else:
         share_test = 0.2
     return share_test
+
+
+def get_config(config: Path) -> dict:
+    """Return config from config file"""
+    return yaml.load(Path(config).open("r"), Loader=yaml.FullLoader)
+
+
+def get_train_test(
+    classif_df: pd.DataFrame, random_state: str = 42
+) -> Tuple[Any, Any, Any, Any]:
+    """Return arrays of train, test data (features and output)"""
+
+    def get_expansion_level_to_label(x: str) -> int:
+        """Turn the expansion level into a binary var with value 0 if 'SEED' else 0"""
+        return 0 if x == "SEED" else 1
+
+    def get_test_ratio(
+        seed_size: int, min_size: int = 100, threshold_size: int = 250
+    ) -> float:
+        """
+        Return the antiseed size such that the seed never represents less than 10% of the sample between `min_size` and
+        `threshold_size` (linear) and 20% of the sample above `threshold_size` (constant). Seeds with less than `min_size`
+        patents raise an error.
+        """
+        if seed_size < min_size:
+            raise SmallSeed("Danger Zone: your seed is too small. Don't cross!")
+        elif min_size <= seed_size < threshold_size:
+            test_ratio_ = (
+                0.5 - (seed_size - min_size) / (threshold_size - min_size) * 0.3
+            )
+        else:
+            test_ratio_ = 0.2
+        return test_ratio_
+
+    var_required = ["abstract", "expansion_level"]
+
+    for v in var_required:
+        assert v in classif_df.columns
+
+    # prepare labels
+    classif_df["label"] = classif_df["expansion_level"].apply(
+        lambda x: get_expansion_level_to_label(x)
+    )
+    # shuffle
+    classif_df.sample(frac=1, random_state=random_state)
+
+    # train test set
+    X = classif_df["abstract"].to_list()
+    y = classif_df["label"].to_list()
+    test_size = get_test_ratio(len(classif_df.query("expansion_level=='SEED'")))
+    texts_train, texts_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+    return texts_train, texts_test, y_train, y_test
