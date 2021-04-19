f01286548e16ff3ae17443c3b7146faf1a210ddb
Cyril Verluise <cyril.verluise@gmail.com>
2021-04-19 23:34:12 +0200

    :tada: Add UnknownModel Exception

--- techlandscape/exception.py
+++ techlandscape/exception.py
@@ -1,5 +1,12 @@
 SMALL_SEED_MSG = "Danger Zone: your seed is too small. Don't cross!"
+UNKNOWN_MODEL_MSG = (
+    "Unknown model architecture. Make sure that your config file is properly defined."
+)
 
 
 class SmallSeed(Exception):
+    pass
+
+
+class UnknownModel(Exception):
     pass
