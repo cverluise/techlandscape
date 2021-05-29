3f9b28284f7c67f875c356fd1643d65da0b600c3
Cyril Verluise <cyril.verluise@gmail.com>
2021-05-29 19:05:10 +0200

    :tada: add robustness subcommand

--- techlandscape/main.py
+++ techlandscape/main.py
@@ -1,4 +1,12 @@
-from techlandscape import candidates, antiseed, expansion, errors, assets, utils
+from techlandscape import (
+    candidates,
+    antiseed,
+    expansion,
+    robustness,
+    errors,
+    assets,
+    utils,
+)
 import typer
 
 app = typer.Typer()
@@ -6,6 +14,7 @@ app = typer.Typer()
 app.add_typer(candidates.app, name="candidates")
 app.add_typer(antiseed.app, name="antiseed")
 app.add_typer(expansion.app, name="expansion")
+app.add_typer(robustness.app, name="robustness")
 app.add_typer(errors.app, name="model.errors")
 app.add_typer(assets.app, name="assets")
 app.add_typer(utils.app, name="utils")
