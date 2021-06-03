33df57184755db2b0f60d4a2adaf3f81cc265973
Cyril Verluise <cyril.verluise@gmail.com>
2021-06-03 19:14:10 +0200

    :art: refacto and deprecate augmented antiseed. Move antiseed a la AF to main expansion func

--- techlandscape/antiseed.py
+++ techlandscape/antiseed.py
@@ -1,8 +1,6 @@
 import typer
 from pathlib import Path
-from typing import List
 from techlandscape.utils import (
-    get_country_clause,
     get_pc_like_clause,
     get_country_prefix,
     get_project_id,
@@ -10,70 +8,55 @@ from techlandscape.utils import (
 )
 from techlandscape.enumerators import TechClass, PrimaryKey
 
-# TODO work on reproducibility when calling random draw
-#   Could find some inspiration here
-#   https://www.oreilly.com/learning/repeatable-sampling-of-data-sets-in-bigquery-for-machine
-#   -learning
 
 app = typer.Typer()
 
 
-def get_af_antiseed_query(
-    table_ref: str,
-    size: int,
-    src_project_id: str,
-    primary_key: PrimaryKey = PrimaryKey.publication_number,
-    countries: List[str] = None,
-) -> str:
-    """
-    Return the anti-seed a la AF
-    """
-
-    country_prefix = get_country_prefix(primary_key)
-    country_clause = get_country_clause(countries)
-    query = f"""SELECT
-      DISTINCT(r.{primary_key.value}) AS {primary_key.value},
-      "ANTISEED-AF" AS expansion_level
-    FROM
-      `{src_project_id}.google_patents_research.publications` AS r {country_prefix}
-    LEFT OUTER JOIN
-      {table_ref} AS tmp
-    ON
-      r.{primary_key.value} = tmp.{primary_key.value}
-    WHERE
-      r.abstract is not NULL
-      AND r.abstract!=''
-      {country_clause}
-    ORDER BY
-      RAND()
-    LIMIT
-      {size}
-    """
-    return query
-
-
-def get_aug_antiseed_query(
-    table_ref: str,
-    size: int,
+@app.command(deprecated=True)
+def get_aug_antiseed(
+    primary_key: PrimaryKey,
     tech_class: TechClass,
-    pc_list: List[str],
-    src_project_id: str,
-    primary_key: PrimaryKey = PrimaryKey.publication_number,
-    countries: List[str] = None,
-) -> str:
+    pc_list: str,
+    size: int,
+    table_ref: str,
+    destination_table: str,
+    credentials: Path,
+    write_disposition: str = "WRITE_APPEND",
+    verbose: bool = False,
+) -> None:
     """
+    !!! warning "Deprecated"
+        The augmented antiseed is not used any more. We have "close" negative examples from human annotations.
+
     Return the augmented anti-seed
+
+    Arguments:
+        primary_key: table primary key
+        tech_class: technological class considered
+        pc_list: list of technological classes to draw from, comma-separated (e.g. A,B,C)
+        size: size of the antiseed
+        table_ref: expansion table (project.dataset.table)
+        destination_table: query results destination table (project.dataset.table)
+        credentials: credentials file path
+        write_disposition: BQ write disposition
+        verbose: verbosity
+
+    **Usage:**
+        ```shell
+        techlandscape antiseed get-af-antiseed family_id cpc Y12,Y10 300 <expansion-table> <destination-table> credentials_bq.json
+        ```
     """
+    project_id = get_project_id(primary_key, credentials)
+    pc_list = pc_list.split(",")
 
     pc_like_clause_ = get_pc_like_clause(tech_class, pc_list, sub_group=True)
     country_prefix = get_country_prefix(primary_key)
-    country_clause = get_country_clause(countries)
     query = f"""
     SELECT
       DISTINCT(r.{primary_key.value}) AS {primary_key.value},
       "ANTISEED-AUG" AS expansion_level
     FROM
-      `{src_project_id}.google_patents_research.publications` AS r,
+      `{project_id}.patents.publications` AS r,
       UNNEST({tech_class.value}) AS {tech_class.value} {country_prefix}
     LEFT OUTER JOIN
       {table_ref} AS tmp
@@ -81,48 +64,12 @@ def get_aug_antiseed_query(
       r.{primary_key.value} = tmp.{primary_key.value}
     WHERE
       {pc_like_clause_}
-      {country_clause}
-      AND r.abstract is not NULL
-      AND r.abstract!=''
     ORDER BY
       RAND()
     LIMIT
       {size}
     """
-    return query
-
-
-# @monitor
-@app.command()
-def get_antiseed(
-    table_ref: str,
-    destination_table: str,
-    size: int,
-    tech_class: TechClass,
-    pc_list: List[str],
-    credentials: Path,
-    primary_key: PrimaryKey = PrimaryKey.publication_number,
-    countries: List[str] = None,
-):
-    """
-    Draw antiseed (a la AF & augmented)
-    """
-
-    src_project_id = get_project_id(primary_key, credentials)
-    af_antiseed_query = get_af_antiseed_query(
-        table_ref, size, src_project_id, primary_key=primary_key, countries=countries
-    )
-    aug_antiseed_query = get_aug_antiseed_query(
-        table_ref,
-        size,
-        tech_class,
-        pc_list,
-        src_project_id,
-        primary_key=primary_key,
-        countries=countries,
-    )
-    get_bq_job_done(af_antiseed_query, destination_table, credentials)
-    get_bq_job_done(aug_antiseed_query, destination_table, credentials, "WRITE_APPEND")
+    get_bq_job_done(query, destination_table, credentials, write_disposition, verbose)
 
 
 if __name__ == "__main__":
