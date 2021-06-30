:bug: fix Error (tf.nn.softmax applied to pred object instead of tensor, ie pred['logits'])

techlandscape/robustness.py
@@ -297,7 +297,7 @@ def get_prediction_analysis(models: str, data: str, destination: Path = None):
 
         pred = model.predict(text_vectorizer.x_test, batch_size=100)
         if cfg["model"]["architecture"] == SupportedModels.transformers.value:
-            score = tf.nn.softmax(pred)
+            score = tf.nn.softmax(pred["logits"])
             # pred is n*2, each line is [logits_0, logits_1]. We transform to proba (score) using softmax
             pred = score[:, 1].numpy()
             # we keep only the proba of class 1